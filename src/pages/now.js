79e6a79f2791c1b6f0085747495b3ac0e42764bc
Kyle Jones <kylejones1310@outlook.com>
2020-05-13 14:41:00 +0100

    Formatting using prettier

--- src/pages/now.js
+++ src/pages/now.js
@@ -24,87 +24,84 @@ class Now extends React.Component {
 
     return (
       <StaticQuery
-      query={nowQuery}
-      render={data => {
-                        const book = data.goodreadsBook.book
-                        const authors = book.authors
-                        const artist = data.allSpotifyTopArtist.edges[0].node
+        query={nowQuery}
+        render={data => {
+          const book = data.goodreadsBook.book
+          const authors = book.authors
+          const artist = data.allSpotifyTopArtist.edges[0].node
 
-                        // TODO: Add logic to add "and" and commas between values
-                        return (
-                          <Layout location={this.props.location}>
-                            <SEO title={title} keywords={keywords} />
-                            <Hero
-                              heroImg={siteConfig.siteCover}
-                              title={title}
-                            />
-                            <Wrapper className={this.props.className}>
-                              <Container className="page-content" fluid>
-                                <Row>
-                                  <ul>
-                                    <li>
-                                      <strong>Reading:</strong>{' '}
-                                      <a
-                                        className={'book-link-${book.isbn}'}
-                                        href={book.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                      >
-                                        {book.titleWithoutSeries}
-                                      </a>{' '}
-                                      by{' '}
-                                      {authors.map(author => (
-                                        <React.Fragment key={author.id}>
-                                          <a
-                                            className={
-                                              'author-link-${author.id}'
-                                            }
-                                            href={author.link}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                          >
-                                            {author.name}
-                                          </a>
-                                        </React.Fragment>
-                                      ))}
-                                    </li>
-                                    <li>
-                                      <strong>Listening to:</strong>{' '}
-                                      <a
-                                        className={'artist-link'}
-                                        href={artist.external_urls.spotify}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                      >
-                                        {artist.name}
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <strong>Watching:</strong> Star Wars:
-                                      Rebels
-                                    </li>
-                                    <li>
-                                      <strong>Learning about:</strong> Gatsby
-                                      and React
-                                    </li>
-                                    <li>
-                                      <strong>Working on:</strong> This website
-                                      and decorating our new home
-                                    </li>
-                                  </ul>
-                                </Row>
-                              </Container>
-                            </Wrapper>
-                          </Layout>
-                        )
-                      }
-}/>)}}
+          // TODO: Add logic to add "and" and commas between values
+          return (
+            <Layout location={this.props.location}>
+              <SEO title={title} keywords={keywords} />
+              <Hero heroImg={siteConfig.siteCover} title={title} />
+              <Wrapper className={this.props.className}>
+                <Container className="page-content" fluid>
+                  <Row>
+                    <ul>
+                      <li>
+                        <strong>Reading:</strong>{' '}
+                        <a
+                          className={'book-link-${book.isbn}'}
+                          href={book.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {book.titleWithoutSeries}
+                        </a>{' '}
+                        by{' '}
+                        {authors.map(author => (
+                          <React.Fragment key={author.id}>
+                            <a
+                              className={'author-link-${author.id}'}
+                              href={author.link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {author.name}
+                            </a>
+                          </React.Fragment>
+                        ))}
+                      </li>
+                      <li>
+                        <strong>Listening to:</strong>{' '}
+                        <a
+                          className={'artist-link'}
+                          href={artist.external_urls.spotify}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {artist.name}
+                        </a>
+                      </li>
+                      <li>
+                        <strong>Watching:</strong> Star Wars: Rebels
+                      </li>
+                      <li>
+                        <strong>Learning about:</strong> Gatsby and React
+                      </li>
+                      <li>
+                        <strong>Working on:</strong> This website and decorating
+                        our new home
+                      </li>
+                    </ul>
+                  </Row>
+                </Container>
+              </Wrapper>
+            </Layout>
+          )
+        }}
+      />
+    )
+  }
+}
 
 export default styled(Now)`
   .page-content {
     max-width: 100%;
     margin-bottom: 40px;
-  }`
+  }
+`
 
 const nowQuery = graphql`
   query NowQuery {
