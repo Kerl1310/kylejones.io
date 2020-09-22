a8c5863c48c5a46291d4f46bf4e93e68b543f769
Kyle <kylejones1310@outlook.com>
2020-09-22 12:35:58 +0100

    Adding initial commit of toggle for dark mode

---
+++ src/components/slider-toggle/slider-toggle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+<label for="checkbox" title="Swap color scheme between dark and light mode" class="dark-mode-toggle">
+    <input type="checkbox" id="checkbox">
+        <div class="slider-round"></div>
+        <div class="toggle-moon">🌙</div>
+        <div class="toggle-sun">☀️</div>
+    </input>
+</label>
