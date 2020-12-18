f3f9891e3b4450aa8399454e6eb5fa671a48c2dd
Kyle <kylejones1310@outlook.com>
2020-12-18 16:52:30 +0000

    New footer added with Buy a Coffee button

---
+++ src/components/footer/footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import CoffeeButton from '../coffeeButton';
+
+const FooterWrapper = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  margin: 0 auto;
+  display: block;
+  width: 100%;
+  z-index: 1000;
+  background-color: #25303b;
+`;
+
+const FooterNav = styled.div`
+  margin-left: auto;
+  margin-right: 3rem;
+  height: 60px;
+  max-width: 960px;
+`;
+
+const FooterLinkGroup = styled.div`
+`;
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <FooterWrapper>
+        <FooterNav>
+          <FooterLinkGroup>
+            <CoffeeButton />
+          </FooterLinkGroup>
+        </FooterNav>
+      </FooterWrapper>
+    );
+  }
+}
+
+export default Footer;
